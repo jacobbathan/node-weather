0e4c934ca087453a977e794be9516e15511a26cd
jacobbathan <jacobbathan@gmail.com>
2019-05-28 22:07:08 -0700

    working on error handling for geocoding

--- app.js
+++ app.js
@@ -3,11 +3,34 @@ const request = require('request');
 const url =
   'https://api.darksky.net/forecast/70bfc6a62dad2e2d4636d3ceced74bee/37.8267,-122.4233';
 
+const mapBoxUrl =
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFjb2JiYXRoYW4iLCJhIjoiY2p3OGtlcG1lMDExYzN5cWpidXl4cjVsZiJ9.Kzxk9L0GScRASwdtR4SAgA&limit=1';
+
+const mapboxKey =
+  'pk.eyJ1IjoiamFjb2JiYXRoYW4iLCJhIjoiY2p3OGtlcG1lMDExYzN5cWpidXl4cjVsZiJ9.Kzxk9L0GScRASwdtR4SAgA';
+
 request({url: url, json: true}, (error, response) => {
-  const temp = response.body.currently.temperature;
-  const rainChance = response.body.currently.precipProbability;
-  // console.log(response.body.currently);
-  console.log(response.body.daily.data[0].summary);
-  console.log('It is currently ' + temp + ' degrees outside.');
-  console.log('There is a ' + rainChance + '% chance of rain today.');
+  if (error) {
+    console.log('unable to connect to weather service');
+  } else if (response.body.error) {
+    console.log('Unable to find location');
+  } else {
+    const temp = response.body.currently.temperature;
+    const rainChance = response.body.currently.precipProbability;
+    console.log(response.body.daily.data[0].summary);
+    console.log('It is currently ' + temp + ' degrees outside.');
+    console.log('There is a ' + rainChance + '% chance of rain today.');
+  }
+});
+
+request({url: mapBoxUrl, json: true}, (error, response) => {
+  if (error) {
+    console.log('Unable to access geocoding location');
+  } else if (response.body.features.length === 0) {
+    console.log('Unable to geocode location');
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log('Coordinates: ' + latitude + ' ' + longitude);
+  }
 });
