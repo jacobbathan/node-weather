8b64670ffefc92be90035678cca2025c8021a02c
jacobbathan <jacobbathan@gmail.com>
2019-05-30 17:18:18 -0700

    user input

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const request = require('request');
 const geocode = require('./utils/geocode');
-const forecast = require('./utils/forecast')
+const forecast = require('./utils/forecast');
 
-forecast(-75.7088, 44.1415, (error, data) => {
-  console.log('Error:', error);
-  console.log('Data:', data);
-});
+// Accept location via command line argument
+//
+//
+//Access command line argument without yargs
+//use string valuse as input for geocode
+//only geocode if location was provided
+//test work with some locations
 
-geocode('Philadelphia', (error, data) => {
-  console.log('Error:', error);
-  console.log('Data:', data);
+
+
+geocode(process.argv[2], (error, data) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error);
+    }
+      // console.log(process.argv[2])
+      console.log(data.location)
+      console.log(forecastData)
+  });
 });
