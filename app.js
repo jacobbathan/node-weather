5f7f22dc9fe0aa28de343d14fa6926a9747c3b5e
jacobbathan <jacobbathan@gmail.com>
2019-05-30 18:43:35 -0700

    refactored, here we go express YAYEET

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-const request = require('request');
-const geocode = require('./utils/geocode');
-const forecast = require('./utils/forecast');
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-// Accept location via command line argument
-//
-//
-//Access command line argument without yargs
-//use string valuse as input for geocode
-//only geocode if location was provided
-//test work with some locations
-
-
-
-geocode(process.argv[2], (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
-
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+const address = process.argv[2];
+if (!address) {
+  console.log("Please provide an address");
+} else {
+  geocode(address, (error, { latitude, longitude, location }) => {
     if (error) {
       return console.log(error);
     }
-      // console.log(process.argv[2])
-      console.log(data.location)
-      console.log(forecastData)
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(location);
+      console.log(forecastData);
+    });
   });
-});
+}
